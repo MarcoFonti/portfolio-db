54868b528f521abb3ab802eaa746f1bc0efa8661
MarcoFonti <theemporiumg@gmail.com>
2024-03-31 00:21:37 +0100

    add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Portfolio::factory(10)->create();
     }
 }
